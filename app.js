b948cce843d76d0eefce11e02436568878f9399b
huseyin <huseyinarslan06@gmail.com>
2022-06-06 14:23:20 +0300

    second step on js

--- app.js
+++ app.js
@@ -2,50 +2,50 @@ let randomNumber = Math.floor(Math.random() * 100 + 1);
 console.log(randomNumber);
 
 let guess = document.getElementById("user-input");
-console.log(guess);
+// console.log(guess);
 
 let check = document.getElementById("check-button");
 
-let restart = document.getElementById("start");
+let restart = document.getElementById("restart");
 
 let warning = document.getElementById("remaining");
 
 let attempt = document.getElementById("attempt");
-// console.log(attempt);
-// let attempt = 5;
-// console.log(attempt);
-
-
-// console.log(warning);
 
 
 const guessGame = function () {
-    if (attempt.innerText > 1) {
-        
+    if (attempt.innerText >= 1) {
+        attempt.innerText--;
 
         if (guess.value > 100 || guess.value < 1) {
             warning.innerText = "Please enter a valid number"
             
         } else {
-            if (guess.value < randomNumber) {
-            warning.innerText = "increase";
-            attempt.innerText--;
-         
-            } else if (guess.value > randomNumber) {
-                warning.innerText = "decrease";
-                attempt.innerText--;
-
-            } else if (guess.value == randomNumber) {
+            if (guess.value == randomNumber) {
                 warning.innerText = "Congratulations"
-                attempt.innerText--;
+                // attempt.innerText--;
+            
+            } else if (guess.value > randomNumber) {
+                warning.innerText = "Decrease the number you entered";
+                // attempt.innerText--;
+
+            } else if (guess.value < randomNumber) {
+            warning.innerText = "Increase the number you entered";
+            // attempt.innerText--;
             }
         
         } 
-} else {
-    warning.innerText = "Game over"
-    attempt.innerText--;
+    } else {
+        warning.innerText = "Sorry, You lost the game. Game Over!"
+        // attempt.innerText--;
     };
 }
     
 check.addEventListener("click", guessGame); 
+check.addEventListener("keydown", (e) => {
+    if (e.code === 'Enter') {
+        guessGame();
+    }
+})
 
+restart.addEventListener("click", guessGame);
